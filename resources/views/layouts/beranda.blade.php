c57d91469a19f209d2c336e4e4b1e5d2210c1c18
widyawwww <widyasismayani03@gmail.com>
2024-07-05 12:49:25 +0700

    widya

--- resources/views/layouts/beranda.blade.php
+++ resources/views/layouts/beranda.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<!-- Created By CodingLab - www.codinglabweb.com -->
 <html lang="en" dir="ltr">
-  <head>
-   <meta charset="UTF-8">
-   <title>Navigation Bar With Scroll Every Section</title>
- <link rel="stylesheet" href="style.css">
-  <!-- Fontawesome CDN Link -->
+<head>
+    <meta charset="UTF-8">
+    <title>Navigation Bar With Scroll Every Section</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Fontawesome CDN Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href="{{ asset('css/beranda.css') }}" rel="stylesheet" />
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-   </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/beranda.css') }}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
 <body>
+
+  <!-- Navbar -->
   <nav>
     <div class="navbar">
       <div class="logo"><a href="#">DKM</a></div>
@@ -24,13 +25,25 @@
       </ul>
     </div>
   </nav>
-  <section id="Home">Home Section</section>
-  <section id="About">About Section</section>
-  <section id="Category">Category Section</section>
-  <section id="Contact">Contact Section</section>
-  <section id="Feedback">Feedback Section</section>
-  <div class="button">
-    <a href="#Home"><i class="fas fa-arrow-up"></i></a>
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <!-- Gambar -->
+  <div class="centered-image">
+    <img src="{{ asset('image/sahpiw.jpg') }}" alt="">
   </div>
+
+  <div class="card ">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+
+  <!-- jQuery, Popper.js, and Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
