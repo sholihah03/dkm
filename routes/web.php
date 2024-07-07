9c91ca1f67cf57ae67c3cf86a18d31d16c1a13cf
aprill18 <apriliadyrahman8@gmail.com>
2024-07-07 20:58:49 +0700

    kontak

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\beranda;
 use App\Http\Controllers\settingaccountController;
+use App\Http\Controllers\ContactController;
 // use App\Http\Controllers\settings\settingaccountController;
 
 Route::get('/', function () {
@@ -19,5 +20,6 @@ Route::get('/homepage',[beranda::class, 'index']);
 
 // Route::get('/settingAkun', [settingaccountController::class, 'index']);
 Route::get('/settingAkun', [settingaccountController::class, 'index2']);
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 // Route::get('/settingAkun', [settingaccountController::class, 'index3']);
 // Route::view('/homepage','layouts.beranda1')->name('beranda1');
