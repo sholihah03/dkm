7d8d7bba7668757b5d549f07980618179aa134fb
widyawwww <widyasismayani03@gmail.com>
2024-07-09 10:24:15 +0700

    push bagian admin

--- database/migrations/2024_07_09_025940_create_tabungankurban_table.php.php
+++ database/migrations/2024_07_09_025940_create_tabungankurban_table.php.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('tabungankurban', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('pesan');
     }
 };
